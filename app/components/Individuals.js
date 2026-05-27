25d6e57341938e1618fb1fa44c64bbf8a2cb4473
khushal1203 <vorakhushal482@gmail.com>
2026-05-27 11:58:00 +0530

    fix the mobile view issue

--- app/components/Individuals.js
+++ app/components/Individuals.js
@@ -1,29 +1,75 @@
 "use client";
-import Slider from "react-slick";
+import { useRef, useState, useEffect } from "react";
+import dynamic from "next/dynamic";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+const Slider = dynamic(() => import("react-slick"), { ssr: false });
+
 const slides = [1, 2, 3, 4, 5];
 
+const slickSettings = {
+  infinite: true,
+  centerMode: true,
+  centerPadding: "100px",
+  slidesToShow: 3,
+  speed: 800,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows: false,
+  dots: false,
+  pauseOnHover: false,
+  cssEase: "ease-in-out",
+  responsive: [
+    { breakpoint: 1280, settings: { slidesToShow: 3, centerPadding: "60px" } },
+    { breakpoint: 1024, settings: { slidesToShow: 3, centerPadding: "20px" } },
+  ],
+};
+
 export default function Individuals() {
-  const settings = {
-    infinite: true,
-    centerMode: true,
-    centerPadding: "100px",
-    slidesToShow: 3,
-    speed: 800,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    arrows: false,
-    dots: false,
-    pauseOnHover: false,
-    cssEase: "ease-in-out",
-    responsive: [
-      { breakpoint: 1280, settings: { slidesToShow: 3, centerPadding: "60px" } },
-      { breakpoint: 1024, settings: { slidesToShow: 3, centerPadding: "20px" } },
-      { breakpoint: 768,  settings: { slidesToShow: 1, centerMode: true, centerPadding: "60px" } },
-      { breakpoint: 425,  settings: { slidesToShow: 1, centerMode: false, centerPadding: "0px", infinite: false, swipe: true, draggable: true } },
-    ],
+  const scrollRef = useRef(null);
+  const [active, setActive] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+  const autoScrollRef = useRef(null);
+
+  useEffect(() => {
+    const check = () => setIsMobile(window.innerWidth <= 768);
+    check();
+    window.addEventListener("resize", check);
+    return () => window.removeEventListener("resize", check);
+  }, []);
+
+  useEffect(() => {
+    if (!isMobile) return;
+    const el = scrollRef.current;
+    if (!el) return;
+
+    const startAutoScroll = () => {
+      autoScrollRef.current = setInterval(() => {
+        const slideWidth = el.querySelector(".ind-mobile-slide")?.offsetWidth + 16;
+        const maxScroll = el.scrollWidth - el.offsetWidth;
+        const next = el.scrollLeft + slideWidth > maxScroll ? 0 : el.scrollLeft + slideWidth;
+        el.scrollTo({ left: next, behavior: "smooth" });
+      }, 2500);
+    };
+
+    const stopAutoScroll = () => clearInterval(autoScrollRef.current);
+
+    startAutoScroll();
+    el.addEventListener("touchstart", stopAutoScroll);
+    el.addEventListener("touchend", () => setTimeout(startAutoScroll, 3000));
+
+    return () => {
+      stopAutoScroll();
+      el.removeEventListener("touchstart", stopAutoScroll);
+    };
+  }, [isMobile]);
+
+  const handleScroll = () => {
+    const el = scrollRef.current;
+    if (!el) return;
+    const slideWidth = el.querySelector(".ind-mobile-slide")?.offsetWidth + 16;
+    setActive(Math.round(el.scrollLeft / slideWidth));
   };
 
   return (
@@ -42,18 +88,36 @@ export default function Individuals() {
         </p>
       </div>
 
-      <div className="ind-slider-wrapper">
-        <Slider {...settings}>
-          {slides.map((num) => (
-            <div key={num} className="ind-slide-item">
-              <div className="ind-phone-frame">
+      {isMobile ? (
+        <>
+          <div className="ind-mobile-scroll" ref={scrollRef} onScroll={handleScroll}>
+            {slides.map((num) => (
+              <div key={num} className="ind-mobile-slide">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img src={`/images/mobile/mobile${num}.png`} alt={`Mobile screen ${num}`} />
               </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
+            ))}
+          </div>
+          <div className="ind-mobile-dots">
+            {slides.map((_, i) => (
+              <span key={i} className={`ind-dot${i === active ? " ind-dot--active" : ""}`} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="ind-slider-wrapper">
+          <Slider {...slickSettings}>
+            {slides.map((num) => (
+              <div key={num} className="ind-slide-item">
+                <div className="ind-phone-frame">
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img src={`/images/mobile/mobile${num}.png`} alt={`Mobile screen ${num}`} />
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      )}
     </section>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Perfxt",
@@ -12,16 +13,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&family=Inter:wght@400;500&display=swap"
         />
-        <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1914651325815360');fbq('track','PageView');` }} />
-        <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1914651325815360&ev=PageView&noscript=1" />` }} />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1914651325815360');fbq('track','PageView');`}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display:"none"}} src="https://www.facebook.com/tr?id=1914651325815360&ev=PageView&noscript=1" alt="" />
+        </noscript>
+      </body>
     </html>
   );
 }
