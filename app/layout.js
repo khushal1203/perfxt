534583a001b0b71332307deead5b79e3ef0fca4a
khushal1203 <vorakhushal482@gmail.com>
2026-05-28 11:22:05 +0530

    fix the build issue and rebuild

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,7 @@ export const metadata = {
   },
 };
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning className={`${outfit.variable} ${inter.variable}`}>
