5130c0ffe7e467a12364d32a798dbd300419b6d5
khushal1203 <vorakhushal482@gmail.com>
2026-05-28 10:27:23 +0530

    fix all issue and add favicon icon

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ export const metadata = {
   title: "Perfxt",
   description: "Perfxt - Landing Page",
   icons: {
-    icon: "/images/home/fvicon.jpeg",
+    icon: "/favicon.png",
   },
 };
 
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
